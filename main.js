1ec573b3626d3c3c3bd3fee4b12fe86b629a2800
HCK <13456323892@163.com>
2016-04-15 21:48:45 +0800

    create by hck

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 /**
  * Created by 王彪 on 2016/4/13.
  */
+/*HCK modify */
+function myf(){
+    var s = document.getElementById("gp").value;//获取搜索框中的值
+
+    if(s.length!=0)
+    {
+        var reg = /^[\u4e00-\u9fa5^0-9]+$/;//只能输入中文和数字
+        if(!reg.test(s))					//判断搜索框中是不是中文和数字
+        {
+            alert("请正确输入股票名或股票代码！")
+        }
+        else
+        {
+
+        }
+    }
+    else{
+        alert("请输入股票名或股票代码！")
+    }
+}
